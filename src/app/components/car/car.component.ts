a99a5c492444c80df6d7746b139855667cc747fc
Mahomed Akhmedov <mahomed_akhmedov@epam.com>
2020-12-25 14:53:00 +0200

    [Components] Add new car component and clean app.component

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  // On component init (earlier)
+  constructor() {
+  }
+
+  // On component init
+  ngOnInit(): void {
+  }
+
+}
