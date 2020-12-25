b546e15fa994cf018fe69551eb26fd381257e013
Mahomed Akhmedov <mahomed_akhmedov@epam.com>
2020-12-25 15:33:23 +0200

    [Car] Add some fields to component

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -7,12 +7,33 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
+  name: string;
+  speed: number;
+  model: string;
+  colors: Colors;
+  options: string[];
+
   // On component init (earlier)
   constructor() {
+    this.name = 'Audi';
+    this.speed = 235;
+    this.model = 'RS8';
+    this.colors = {
+      car: 'white',
+      salon: 'black',
+      wheels: 'silver'
+    };
+    this.options = ['ABS', 'Auto', 'Parking'];
   }
 
   // On component init
   ngOnInit(): void {
   }
 
+}
+
+interface Colors {
+  car: string;
+  salon: string;
+  wheels: string;
 }
