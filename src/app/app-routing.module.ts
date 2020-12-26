7f16d0bbf7d3c47d6ded063385a18f1831cfd2c8
Mahomed Akhmedov <mahomed_akhmedov@epam.com>
2020-12-26 17:20:03 +0200

    [Navbar] Implement navbar and init other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {IndexComponent} from './components/index/index.component';
+import {EventComponent} from './components/event/event.component';
+import {HumanComponent} from './components/human/human.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'events', component: EventComponent},
+  {path: 'humans', component: HumanComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavComponent} from './components/nav/nav.component';
+import { IndexComponent } from './components/index/index.component';
+import { EventComponent } from './components/event/event.component';
+import { HumanComponent } from './components/human/human.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    IndexComponent,
+    EventComponent,
+    HumanComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
